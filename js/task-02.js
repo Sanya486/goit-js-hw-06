10430cf29bd147a76763e3707477236c92c89b04
Alex_N <120394126+Sanya486@users.noreply.github.com>
2023-04-13 17:20:59 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,32 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('#ingredients');
+
+const firstIngredient = document.createElement('li');
+firstIngredient.classList.add('item');
+firstIngredient.textContent = ingredients[0];
+
+const secoondIngredient = document.createElement('li');
+secoondIngredient.classList.add('item');
+secoondIngredient.textContent = ingredients[1];
+
+const thirdIngredient = document.createElement('li');
+thirdIngredient.classList.add('item');
+thirdIngredient.textContent = ingredients[2];
+
+const fourthIngredient = document.createElement('li');
+fourthIngredient.classList.add('item');
+fourthIngredient.textContent = ingredients[3];
+
+const fifthIngredient = document.createElement('li');
+fifthIngredient.classList.add('item');
+fifthIngredient.textContent = ingredients[4];
+
+const sixthIngredient = document.createElement('li');
+sixthIngredient.classList.add('item');
+sixthIngredient.textContent = ingredients[5];
+
+
+listEl.append(firstIngredient, secoondIngredient, thirdIngredient, fourthIngredient, fifthIngredient, sixthIngredient);
