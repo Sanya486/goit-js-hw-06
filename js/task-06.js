75e00094d4947843c77f230adde8bb288ae9d45c
Alex_N <120394126+Sanya486@users.noreply.github.com>
2023-04-13 23:46:35 +0300

    task 6

---
+++ js/task-06.js
@@ -0,0 +1,18 @@
+const inputEl = document.querySelector('#validation-input');
+
+inputEl.addEventListener('blur', (event) => {
+
+    if (+inputEl.dataset.length === event.currentTarget.value.length){
+        inputEl.classList.add('valid')
+        inputEl.classList.remove('invalid')
+    }
+
+    else if (+inputEl.dataset.length !== event.currentTarget.value.length && event.currentTarget.value.length > 0){
+        inputEl.classList.add('invalid');
+        inputEl.classList.remove('valid');
+    }
+    else {
+        inputEl.classList.remove('invalid');
+        inputEl.classList.remove('valid');
+    }
+})
