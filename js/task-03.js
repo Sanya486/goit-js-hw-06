ad45516e6ab79baebc433208095e4da8f62998e9
Alex_N <120394126+Sanya486@users.noreply.github.com>
2023-04-13 18:01:34 +0300

    task 3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,9 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryEl = document.querySelector('.gallery');
+
+const markup = images.map( ({url, alt}) => `<li><img src="${url}" alt="${alt}"></li>`).join('');
+
+galleryEl.insertAdjacentHTML("afterbegin", markup);
